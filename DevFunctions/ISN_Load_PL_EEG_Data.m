4b5db49893fb95afbb23fe29449ac34cf42ab904
eddyrhodes <38327639+eddyrhodes@users.noreply.github.com>
2019-05-16 23:31:35 +0100

    Add files via upload

--- DevFunctions/ISN_Load_PL_EEG_Data.m
+++ DevFunctions/ISN_Load_PL_EEG_Data.m
@@ -1,7 +1,7 @@
 function [dataArray] = ISN_Load_PL_EEG_Data(filename)
-%ISN_Load_PL_EEG_Data Loads the data from the output .csv file from our PL_EEG system 
-%   [dataArray] = ISN_Load_PL_EEG_Data(filename); Current Firmware: 190307 
-%   This function takes into account the three different firmware used to 
+%ISN_Load_PL_EEG_Data Loads the data from the output .csv file from our PL_EEG system
+%   [dataArray] = ISN_Load_PL_EEG_Data(filename); Current Firmware: 190307
+%   This function takes into account the three different firmware used to
 %   create EEG files so far. The output variable always has the following
 %   order: (1) Abs time, (2) Mode, (3) DT, (4) EEG, (5) Stim_out
 
@@ -39,6 +39,15 @@ dataArray = [dataArray2{1:end-1}];
 if numCols == 7
     tmp = [dataArray(:,1), dataArray(:,5), dataArray(:,4), dataArray(:,2), dataArray(:,3)];
     dataArray = tmp;
+    % Firmware version 190307 now contains a log - @echo, these lines do
+    % not contain data and have been removed
+elseif numCols == 9
+    for n = size(dataArray,2):-1:1
+        tmp(:,n) = isnan(dataArray(:,n));
+    end
+    tmp2=sum(tmp,2);
+    tmp3 = tmp2==0;
+    dataArray = dataArray(tmp3,:);
 end
 
 end
