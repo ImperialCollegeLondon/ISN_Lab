9c187ad6edf8a56922f1330f7240d44ab9ec55fa
LegendLukaz <lukasfan08@gmail.com>
2019-06-22 12:50:49 +0100

    moved index calculation out of the loop

--- DevFunctions/Frequency Multiplexing/eegSleepScorePSD.m
+++ DevFunctions/Frequency Multiplexing/eegSleepScorePSD.m
@@ -43,12 +43,12 @@ max_pxx_f = 30;
 
 % Temporary variables
 tmp_pxx = [];
-pxx = [];
-f = [];
+pxx = cell(1,eeg.nc);
 tmp_f = [];
 
 nfft = 2^(nextpow2(eeg.ep_sz(1)));
-
+% set idx for the 30 Hz mark
+idx = round(max_pxx_f/(Fs/nfft),-1);
 for ch=1:eeg.nc
     % compute power density spectrum at epochs
     fprintf('Evaluating channel %d\n',ch);
@@ -58,8 +58,6 @@ for ch=1:eeg.nc
         yy = y_eeg(epoch_idx{i},ch);
         % compute multitaper power spectral density (dB/Hz) in epoch
         [tmp_pxx,tmp_f] = pmtm(yy,3,nfft, Fs); % length of yy may vary per epoch
-        % set idx for the 30 Hz mark
-        idx = round(max_pxx_f/(Fs/nfft),-1);
         pxx{ch}(i,:) = tmp_pxx(1:idx);
         % normalized to total power
         if(normalized_psd_to_total_power)
@@ -74,6 +72,7 @@ for ch=1:eeg.nc
 end
 
 f = tmp_f(1:idx);
+
 
 %% Store into variable
 psd.nc = eeg.nc;
